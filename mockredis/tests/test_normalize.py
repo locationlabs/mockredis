6d181b217a8d3b37049e54540e214e18b61f7343
Jesse Myers <jesse@locationlabs.com>
2014-04-09 10:16:17 -0700

    Extend call normalization to work for zrevrangebyscore. Add tests.

---
+++ mockredis/tests/test_normalize.py
@@ -0,0 +1,67 @@
+"""
+Test redis command normalization.
+"""
+from nose.tools import eq_
+
+from mockredis.client import MockRedis
+
+
+def test_normalize_command_name():
+    cases = [
+        ("del", "delete"),
+        ("ping", "ping"),
+    ]
+
+    def _test(command, expected):
+        redis = MockRedis()
+        eq_(redis._normalize_command_name(command), expected)
+
+    for command, expected in cases:
+        yield _test, command, expected
+
+
+def test_normalize_command_args():
+
+    cases = [
+        (False, "zadd", ("key", "member", 1.0), ("key", 1.0, "member")),
+        (True, "zadd", ("key", 1.0, "member"), ("key", 1.0, "member")),
+
+        (True, "zrevrangebyscore",
+         ("key", "inf", "-inf"),
+         ("key", "inf", "-inf")),
+
+        (True, "zrevrangebyscore",
+         ("key", "inf", "-inf", "limit", 0, 10),
+         ("key", "inf", "-inf", 0, 10, False)),
+
+        (True, "zrevrangebyscore",
+         ("key", "inf", "-inf", "withscores"),
+         ("key", "inf", "-inf", None, None, True)),
+
+        (True, "zrevrangebyscore",
+         ("key", "inf", "-inf", "withscores", "limit", 0, 10),
+         ("key", "inf", "-inf", 0, 10, True)),
+    ]
+
+    def _test(strict, command, args, expected):
+        redis = MockRedis(strict=strict)
+        eq_(tuple(redis._normalize_command_args(command, *args)), expected)
+
+    for strict, command, args, expected in cases:
+        yield _test, strict, command, args, expected
+
+
+def test_normalize_command_response():
+
+    cases = [
+        ("get", "foo", "foo"),
+        ("zrevrangebyscore", [(1, 2), (3, 4)], [1, 2, 3, 4]),
+    ]
+
+    def _test(command, response, expected):
+        redis = MockRedis()
+        eq_(redis._normalize_command_response(command, response), expected)
+
+    for command, response, expected in cases:
+        yield _test, command, response, expected
+
