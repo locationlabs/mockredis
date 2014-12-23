340d6d429810b0c4edc93efc8f24e4d976aaadf7
xiaogaozi <gaochangjian@gmail.com>
2014-12-23 21:03:54 +0800

    Support `scan_iter`, `sscan_iter`, `zscan_iter`, `hscan_iter`

--- mockredis/tests/test_scan.py
+++ mockredis/tests/test_scan.py
@@ -25,6 +25,26 @@ class TestRedisEmptyScans(object):
         eq_scan(self.redis.zscan("foo"), 0, [])
         eq_scan(self.redis.hscan("foo"), 0, {})
 
+        keys = []
+        for k in self.redis.scan_iter():
+            keys.append(k)
+        eq_(keys, [])
+
+        members = []
+        for m in self.redis.sscan_iter('foo'):
+            members.append(m)
+        eq_(members, [])
+
+        members = []
+        for m in self.redis.zscan_iter('foo'):
+            members.append(m)
+        eq_(members, [])
+
+        members = []
+        for m in self.redis.hscan_iter('foo'):
+            members.append(m)
+        eq_(members, [])
+
 
 class TestRedisScan(object):
     """SCAN tests"""
@@ -60,21 +80,37 @@ class TestRedisScan(object):
         eq_(do_full_scan('*abc*', 1), abc_keys)
         eq_(do_full_scan('*abc*', 2), abc_keys)
         eq_(do_full_scan('*abc*', 10), abc_keys)
+        keys = set()
+        for k in self.redis.scan_iter('*abc*'):
+            keys.add(k)
+        eq_(keys, abc_keys)
 
         xyz_keys = set([b'key_xyz_1', b'key_xyz_2', b'key_xyz_3', b'key_xyz_4', b'key_xyz_5'])
         eq_(do_full_scan('*xyz*', 1), xyz_keys)
         eq_(do_full_scan('*xyz*', 2), xyz_keys)
         eq_(do_full_scan('*xyz*', 10), xyz_keys)
+        keys = set()
+        for k in self.redis.scan_iter('*xyz*'):
+            keys.add(k)
+        eq_(keys, xyz_keys)
 
         one_keys = set([b'key_abc_1', b'key_xyz_1'])
         eq_(do_full_scan('*_1', 1), one_keys)
         eq_(do_full_scan('*_1', 2), one_keys)
         eq_(do_full_scan('*_1', 10), one_keys)
+        keys = set()
+        for k in self.redis.scan_iter('*_1'):
+            keys.add(k)
+        eq_(keys, one_keys)
 
         all_keys = abc_keys.union(xyz_keys)
         eq_(do_full_scan('*', 1), all_keys)
         eq_(do_full_scan('*', 2), all_keys)
         eq_(do_full_scan('*', 10), all_keys)
+        keys = set()
+        for k in self.redis.scan_iter('*'):
+            keys.add(k)
+        eq_(keys, all_keys)
 
 
 class TestRedisSScan(object):
@@ -111,21 +147,37 @@ class TestRedisSScan(object):
         eq_(do_full_scan('key', '*abc*', 1), abc_members)
         eq_(do_full_scan('key', '*abc*', 2), abc_members)
         eq_(do_full_scan('key', '*abc*', 10), abc_members)
+        members = set()
+        for m in self.redis.sscan_iter('key', '*abc*'):
+            members.add(m)
+        eq_(members, abc_members)
 
         xyz_members = set([b'xyz_1', b'xyz_2', b'xyz_3', b'xyz_4', b'xyz_5'])
         eq_(do_full_scan('key', '*xyz*', 1), xyz_members)
         eq_(do_full_scan('key', '*xyz*', 2), xyz_members)
         eq_(do_full_scan('key', '*xyz*', 10), xyz_members)
+        members = set()
+        for m in self.redis.sscan_iter('key', '*xyz*'):
+            members.add(m)
+        eq_(members, xyz_members)
 
         one_members = set([b'abc_1', b'xyz_1'])
         eq_(do_full_scan('key', '*_1', 1), one_members)
         eq_(do_full_scan('key', '*_1', 2), one_members)
         eq_(do_full_scan('key', '*_1', 10), one_members)
+        members = set()
+        for m in self.redis.sscan_iter('key', '*_1'):
+            members.add(m)
+        eq_(members, one_members)
 
         all_members = abc_members.union(xyz_members)
         eq_(do_full_scan('key', '*', 1), all_members)
         eq_(do_full_scan('key', '*', 2), all_members)
         eq_(do_full_scan('key', '*', 10), all_members)
+        members = set()
+        for m in self.redis.sscan_iter('key', '*'):
+            members.add(m)
+        eq_(members, all_members)
 
 
 class TestRedisZScan(object):
@@ -162,21 +214,37 @@ class TestRedisZScan(object):
         eq_(do_full_scan('key', '*abc*', 1), abc_members)
         eq_(do_full_scan('key', '*abc*', 2), abc_members)
         eq_(do_full_scan('key', '*abc*', 10), abc_members)
+        members = set()
+        for m in self.redis.zscan_iter('key', '*abc*'):
+            members.add(m)
+        eq_(members, abc_members)
 
         xyz_members = set([(b'xyz_1', 1), (b'xyz_2', 2), (b'xyz_3', 3), (b'xyz_4', 4), (b'xyz_5', 5)])
         eq_(do_full_scan('key', '*xyz*', 1), xyz_members)
         eq_(do_full_scan('key', '*xyz*', 2), xyz_members)
         eq_(do_full_scan('key', '*xyz*', 10), xyz_members)
+        members = set()
+        for m in self.redis.zscan_iter('key', '*xyz*'):
+            members.add(m)
+        eq_(members, xyz_members)
 
         one_members = set([(b'abc_1', 1), (b'xyz_1', 1)])
         eq_(do_full_scan('key', '*_1', 1), one_members)
         eq_(do_full_scan('key', '*_1', 2), one_members)
         eq_(do_full_scan('key', '*_1', 10), one_members)
+        members = set()
+        for m in self.redis.zscan_iter('key', '*_1'):
+            members.add(m)
+        eq_(members, one_members)
 
         all_members = abc_members.union(xyz_members)
         eq_(do_full_scan('key', '*', 1), all_members)
         eq_(do_full_scan('key', '*', 2), all_members)
         eq_(do_full_scan('key', '*', 10), all_members)
+        members = set()
+        for m in self.redis.zscan_iter('key', '*'):
+            members.add(m)
+        eq_(members, all_members)
 
 
 class TestRedisHScan(object):
@@ -213,19 +281,35 @@ class TestRedisHScan(object):
         eq_(do_full_scan('key', '*abc*', 1), abc)
         eq_(do_full_scan('key', '*abc*', 2), abc)
         eq_(do_full_scan('key', '*abc*', 10), abc)
+        data = {}
+        for k, v in self.redis.hscan_iter('key', '*abc*'):
+            data[k] = v
+        eq_(data, abc)
 
         xyz = {b'xyz_1': b'1', b'xyz_2': b'2', b'xyz_3': b'3', b'xyz_4': b'4', b'xyz_5': b'5'}
         eq_(do_full_scan('key', '*xyz*', 1), xyz)
         eq_(do_full_scan('key', '*xyz*', 2), xyz)
         eq_(do_full_scan('key', '*xyz*', 10), xyz)
+        data = {}
+        for k, v in self.redis.hscan_iter('key', '*xyz*'):
+            data[k] = v
+        eq_(data, xyz)
 
         all_1 = {b'abc_1': b'1', b'xyz_1': b'1'}
         eq_(do_full_scan('key', '*_1', 1), all_1)
         eq_(do_full_scan('key', '*_1', 2), all_1)
         eq_(do_full_scan('key', '*_1', 10), all_1)
+        data = {}
+        for k, v in self.redis.hscan_iter('key', '*_1'):
+            data[k] = v
+        eq_(data, all_1)
 
         abcxyz = abc
         abcxyz.update(xyz)
         eq_(do_full_scan('key', '*', 1), abcxyz)
         eq_(do_full_scan('key', '*', 2), abcxyz)
         eq_(do_full_scan('key', '*', 10), abcxyz)
+        data = {}
+        for k, v in self.redis.hscan_iter('key', '*'):
+            data[k] = v
+        eq_(data, abcxyz)
