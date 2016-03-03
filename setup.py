5f106f2e6b03c1919bb08c766bae08bfdc6110eb
Jeremiah Porten <jeremiah@locationlabs.com>
2016-03-02 16:14:52 -0800

    Bumping version after release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 # Match releases to redis-py versions
-__version__ = '2.9.0.12'
+__version__ = '2.9.0.13'
 
 # Jenkins will replace __build__ with a unique value.
 __build__ = ''
